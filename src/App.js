1004a9465d14d5d64eb080c1ab57fe4f08e4f7de
Anjali Srivastava <anjalisri1810@example.com>
2023-10-14 11:31:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './components/Component1.css'
+import './App.css';
+import {Component1} from './components/Component1';
+function App() {
+  return (
+    <div className="App" class="Half">
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Component1.js
@@ -0,0 +1,82 @@
+
+import Axios from "axios";
+import {useEffect,useState} from "react";
+
+export function Component1(){
+    const [data,setData]=useState([]);
+
+    useEffect(()=>{
+        Axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((res)=>{
+            if(res.status===200){
+                setData(res.data);
+            }
+            else{
+                Promise.reject();
+            }
+        })
+        .catch((err)=>alert(err));
+    },[])
+
+    const detail =()=>{
+        return data.map((val)=>{
+            return (<tr>
+                <td>
+                    {val.id}
+                </td>
+                <td>
+                    {val.name}
+                </td>
+                <td>
+                    {val.username}
+                </td>
+              
+                <td>
+                    {val.email}
+                </td>
+                <td>
+                    {val.phone}
+                </td>
+                {/* <td>
+                    {val.ip}
+                </td> */}
+                <td>
+                    {val.website}
+                </td>
+                <td>
+                    {val.address.zipcode}
+                </td>
+                
+            </tr>)
+        })
+    }
+    return(
+        <center>
+        <div>
+            <h1>Dummy Data</h1>
+            <div>
+                <table class="firstTable">
+                    <tr >
+                        <td class="rrow">Sno.   </td>
+                        {/* <td>Profile Pic</td> */}
+                        <td class="rrow">Name   </td>
+                        {/* <td>Last Name</td>
+                        <td>Gender</td> */}
+                        <td class="rrow">E-mail   </td>
+                        <td class="rrow">UserName   </td>
+                        <td class="rrow">Phone No.   </td>
+                        {/* <td>IP</td> */}
+                        <td class="rrow">Website   </td>
+                        <td class="rrow">Pincode   </td>
+                    </tr>
+                    {detail()}
+                </table>
+            </div>
+        </div>
+        </center>
+    )
+
+}
+
+
+
